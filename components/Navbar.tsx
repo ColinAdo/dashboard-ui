db4b9575be5a1cf401c2396614ce87172010820c
ColinAdo <colinadodev30@gmail.com>
2024-06-08 19:44:46 +0300

    Add navbar code

---
+++ components/Navbar.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../img/logo.png";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+// Stateless Function components (sfc)
+const Navbar = () => {
+  return (
+    <div className="bg-primary dark:bg-slate-500 text-white py-2 px-5 flex justify-between">
+      <Link href="/">
+        <Image src={logo} alt="deluxe" width={40} />
+      </Link>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger className="focus:outline-none">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="avatar" />
+            <AvatarFallback className="text-black">CA</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href="/">Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href="#">Blog</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href="/auth">Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default Navbar;
