7c53c9f8fe6fdcea8b7dc09e910b93724ac5d61a
ColinAdo <colinadodev30@gmail.com>
2024-06-12 19:25:13 +0300

    Add props to a commponent for reusability

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
 import DashboardCard from "@/components/dashboard/dashboardCard";
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
-        <DashboardCard />
+        <DashboardCard
+          title="Posts"
+          count={200}
+          icon={<Newspaper className="dark:text-slate-100" />}
+        />
+        <DashboardCard
+          title="Category"
+          count={12}
+          icon={<Folder className="dark:text-slate-100" />}
+        />
+        <DashboardCard
+          title="Users"
+          count={150}
+          icon={<User className="dark:text-slate-100" />}
+        />
+        <DashboardCard
+          title="Comments"
+          count={200}
+          icon={<MessageCircle className="dark:text-slate-100" />}
+        />
       </div>
     </>
   );
--- components/dashboard/dashboardCard.tsx
+++ components/dashboard/dashboardCard.tsx
@@ -1,18 +1,24 @@
 import { Card, CardContent } from "@/components/ui/card";
-import { Newspaper } from "lucide-react";
+import { LucideIcon } from "lucide-react";
 
-const DashboardCard = () => {
+interface DashboardCardProps {
+  title: string;
+  count: number;
+  icon: React.ReactElement<LucideIcon>;
+}
+
+const DashboardCard = ({ title, count, icon }: DashboardCardProps) => {
   return (
     <div>
       <Card className="bg-slate-100 dark:bg-slate-800 justify-start p-4 pb-0">
         <CardContent>
           <h4 className="text-3xl font-bold text-slate-950 dark:text-slate-100 mb-4">
-            Posts
+            {title}
           </h4>
           <div className="flex justify-between">
-            <Newspaper />
+            {icon}
             <h2 className="flex justify-between font-bold dark:text-slate-100 ml-2">
-              100
+              {count}
             </h2>
           </div>
         </CardContent>
