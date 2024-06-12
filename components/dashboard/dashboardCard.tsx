8d4c1f3f48ea63f89dd1e3edb029fa838cf34b85
ColinAdo <colinadodev30@gmail.com>
2024-06-12 19:52:56 +0300

    Add posts data and its type

--- components/dashboard/dashboardCard.tsx
+++ components/dashboard/dashboardCard.tsx
@@ -10,7 +10,7 @@ interface DashboardCardProps {
 const DashboardCard = ({ title, count, icon }: DashboardCardProps) => {
   return (
     <div>
-      <Card className="bg-slate-100 dark:bg-slate-800 justify-start p-4 pb-0">
+      <Card className="bg-slate-100 dark:bg-slate-800 justify-start p-11 pb-0">
         <CardContent>
           <h4 className="text-3xl font-bold text-slate-950 dark:text-slate-100 mb-4">
             {title}
