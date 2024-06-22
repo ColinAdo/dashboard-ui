d611422cf79c3b99349c93521df0a01b34467859
ColinAdo <colinadodev30@gmail.com>
2024-06-22 18:26:53 +0300

    Add auth tabs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Sidebar from "@/components/Sidebar";
-import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/provider/ThemeProvider";
 
